dd894b852b7012bb568706190d99eda06f9a95b7
Hridesh Srivastava <hrideshsrivastavaa@outlook.com>
2024-10-15 15:41:30 +0530

    fix schema issues

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -11,7 +11,6 @@ const userSchema = new Schema(
         email : {
             type : String,
             required : true,
-            unique : true,
             lowercase : true,
             trim : true
         },
